261673e8aab6abe631005947b9451a262173ba3b
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-10-04 15:41:09 -0500

    adding delete functionality

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./Develop/public'));
 
-
 // Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -26,12 +25,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    var newNote = req.body;
+    let newNote = req.body;
 
     notes.push(newNote);
     return res.json(notes);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+})
+
 app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`);
 })
+
